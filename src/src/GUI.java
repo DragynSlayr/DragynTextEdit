ffbe22686cbb6841518769949979df15c71e5b7c
Inderpreet <Inderpreet@Inderpreet-PC>
2014-10-15 01:57:22 -0600

    Major revisions to the font menu

--- src/src/GUI.java
+++ src/src/GUI.java
@@ -6,12 +6,17 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.util.ArrayList;
 import javax.swing.Box;
 import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -22,8 +27,12 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.KeyStroke;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.SimpleAttributeSet;
@@ -45,6 +54,10 @@ public class GUI extends JFrame {
     private static final FileOperations fileOps = new FileOperations();
     private static boolean usedEnter = false;
     private static Color correctColor, incorrectColor;
+    JTextField fontExample;
+    private String fontName = Font.SERIF;
+    private int fontStyle = Font.PLAIN, fontSize = 20;
+    private Font selected = new Font(Font.SERIF, Font.PLAIN, 20);
 
     public GUI() {
         //Set the basics of the frame
@@ -151,12 +164,17 @@ public class GUI extends JFrame {
         fontMenuItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame fontFrame = new JFrame("Font Settings");
+                final ButtonGroup group = new ButtonGroup();
+
+                final JFrame fontFrame = new JFrame("Font Settings");
+                fontFrame.setSize(getWidth(), getHeight());
                 fontFrame.setLocationRelativeTo(null);
-                fontFrame.setSize(150, 150);
                 fontFrame.setVisible(true);
 
-                JPanel panel = new JPanel(new GridLayout(10, 3), true);
+                final JPanel panel = new JPanel(new GridLayout(10, 3), true);
+
+                fontExample = new JTextField();
+                fontExample.setFont(selected);
 
                 JLabel fontLabel = new JLabel("Font Type");
                 panel.add(fontLabel);
@@ -164,7 +182,14 @@ public class GUI extends JFrame {
                 ActionListener buttonListener = new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        System.out.println(e.getSource());
+                        JRadioButton pressed = (JRadioButton) e.getSource();
+                        if (pressed.getActionCommand().equalsIgnoreCase("serif")) {
+                            fontName = Font.SERIF;
+                        } else {
+                            fontName = Font.SANS_SERIF;
+                        }
+                        selected = new Font(fontName, fontStyle, fontSize);
+                        fontExample.setFont(selected);
                     }
                 };
 
@@ -176,16 +201,90 @@ public class GUI extends JFrame {
                 panel.add(serifButton);
                 panel.add(sansSerifButton);
 
-                ButtonGroup group = new ButtonGroup();
                 group.add(serifButton);
                 group.add(sansSerifButton);
 
-                int[] fontStylesInt = {Font.PLAIN, Font.BOLD, Font.ITALIC, Font.BOLD + Font.ITALIC};
+                final JLabel sizeLabel = new JLabel("Font Size: " + fontSize);
+                panel.add(sizeLabel);
+
+                JSlider sizeSlider = new JSlider(JSlider.HORIZONTAL, 10, 100, fontSize);
+                sizeSlider.setMajorTickSpacing(10);
+                sizeSlider.setMinorTickSpacing(5);
+                sizeSlider.setPaintTicks(true);
+                sizeSlider.setPaintLabels(true);
+                sizeSlider.addChangeListener(new ChangeListener() {
+                    @Override
+                    public void stateChanged(ChangeEvent e) {
+                        JSlider change = (JSlider) e.getSource();
+                        fontSize = change.getValue();
+                        selected = new Font(fontName, fontStyle, fontSize);
+                        fontExample.setFont(selected);
+                        sizeLabel.setText("Font Size: " + fontSize);
+                    }
+                });
+
+                panel.add(sizeSlider);
+
                 String[] fontStyles = {"Normal", "Bold", "Italic", "Bold & Italic"};
+                final int[] fontStylesInt = {Font.PLAIN, Font.BOLD, Font.ITALIC, Font.BOLD + Font.ITALIC};
+
+                final JComboBox fontStylesBox = new JComboBox(fontStyles);
+                fontStylesBox.setSelectedIndex(0);
+                fontStylesBox.addItemListener(new ItemListener() {
+                    @Override
+                    public void itemStateChanged(ItemEvent e) {
+                        if (e.getStateChange() == ItemEvent.SELECTED) {
+                            fontStyle = fontStylesInt[fontStylesBox.getSelectedIndex()];
+                            selected = new Font(fontName, fontStyle, fontSize);
+                            fontExample.setFont(selected);
+                        }
+                    }
+                });
+
+                panel.add(fontStylesBox);
+
+                fontExample.setText("Example Text");
+                panel.add(fontExample);
 
                 fontFrame.setContentPane(panel);
+
+                fontFrame.addWindowListener(new WindowListener() {
+
+                    @Override
+                    public void windowOpened(WindowEvent e) {
+                    }
+
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        System.out.println("Closing");
+                        selected = new Font(fontName, fontStyle, fontSize);
+                        textBox.setFont(selected);
+                    }
+
+                    @Override
+                    public void windowClosed(WindowEvent e) {
+                    }
+
+                    @Override
+                    public void windowIconified(WindowEvent e) {
+                    }
+
+                    @Override
+                    public void windowDeiconified(WindowEvent e) {
+                    }
+
+                    @Override
+                    public void windowActivated(WindowEvent e) {
+                    }
+
+                    @Override
+                    public void windowDeactivated(WindowEvent e) {
+                    }
+                });
+
             }
-        });
+        }
+        );
 
         //Create a help JMenu
         JMenu helpMenu = new JMenu("Help");

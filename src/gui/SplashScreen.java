2513be13858202be3de349ffad765e328558b253
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-01-11 22:56:47 -0700

    Added a progress bar to the splash screen

--- src/gui/SplashScreen.java
+++ src/gui/SplashScreen.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.AlphaComposite;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
@@ -12,9 +13,13 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JProgressBar;
 import javax.swing.WindowConstants;
 
 public class SplashScreen {
+
+	private JProgressBar bar;
+	private long startTime;
 
 	public SplashScreen(long time) {
 		// Get the size of the screen
@@ -27,51 +32,43 @@ public class SplashScreen {
 		icon = resizeImageIcon(icon, screenSize.height / 2,
 				screenSize.width / 3, true);
 
-		// Create a new frame
-		JFrame frame = setupUI(
-				new Dimension(icon.getIconWidth(), icon.getIconHeight()), icon);
-
 		// Create a new JLabel to display the image
 		JLabel splash = makeTransparentIconLabel(icon);
 
+		// Create a progress bar
+		bar = new JProgressBar(0, (int) time);
+
+		// Set colors of the bar
+		bar.setForeground(Color.GREEN);
+		bar.setBackground(Color.WHITE);
+
+		// Create a new frame
+		final JFrame frame = setupUI(
+				new Dimension(icon.getIconWidth(), icon.getIconHeight() + bar.getHeight()), icon);
+		
+		// Add bar to frame
+		frame.add(bar, BorderLayout.SOUTH);
+
+		// Create a thread for the progress bar
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (frame.isVisible()) {
+					long now = System.currentTimeMillis();
+					int timeElapsed = (int) (now - startTime);
+					bar.setForeground(Color.GREEN);
+					bar.setBackground(Color.WHITE);
+					bar.setValue(timeElapsed);
+					bar.paint(bar.getGraphics());
+				}
+			}
+		});
+
+		// Start the thread
+		t.start();
+
 		// Display the splash
 		display(time, frame, splash);
-	}
-
-	/**
-	 * Creates a JLabel that is transparent
-	 * 
-	 * @param icon
-	 *            The icon to put on the label
-	 * @return Transparent JLabel
-	 */
-	private JLabel makeTransparentIconLabel(ImageIcon icon) {
-		JLabel label = new JLabel(icon);
-		label.setBackground(new Color(0, 0, 0, 0));
-		label.setOpaque(false);
-		return label;
-	}
-
-	/**
-	 * Sets the parameters of the JFrame
-	 * 
-	 * @param size
-	 *            The size of the label
-	 * @param icon
-	 *            The icon for the frame
-	 * @return A JFrame
-	 */
-	private JFrame setupUI(Dimension size, ImageIcon icon) {
-		JFrame frame = new JFrame("Made By Inderpreet Dhillon");
-		frame.setAlwaysOnTop(true);
-		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		frame.setIconImage(icon.getImage());
-		frame.setResizable(false);
-		frame.setSize(size);
-		frame.setUndecorated(true);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-		return frame;
 	}
 
 	/**
@@ -92,25 +89,10 @@ public class SplashScreen {
 	 *            Label with icon
 	 */
 	private void display(long time, JFrame frame, JLabel splash) {
-		frame.add(splash);
-		frame.setBackground(new Color(0, 0, 0, 0));
+		frame.add(splash, BorderLayout.NORTH);
 		frame.paintAll(frame.getGraphics());
 		pause(time);
 		closeFrame(frame);
-	}
-
-	/**
-	 * Pauses all operations for a time
-	 * 
-	 * @param time
-	 *            Time to pause
-	 */
-	private void pause(long time) {
-		try {
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			System.out.println(e.getMessage());
-		}
 	}
 
 	/**
@@ -128,6 +110,35 @@ public class SplashScreen {
 			System.out.println(e.getMessage());
 		}
 		return null;
+	}
+
+	/**
+	 * Creates a JLabel that is transparent
+	 * 
+	 * @param icon
+	 *            The icon to put on the label
+	 * @return Transparent JLabel
+	 */
+	private JLabel makeTransparentIconLabel(ImageIcon icon) {
+		JLabel label = new JLabel(icon);
+		label.setBackground(Color.BLACK);
+		label.setOpaque(true);
+		return label;
+	}
+
+	/**
+	 * Pauses all operations for a time
+	 * 
+	 * @param time
+	 *            Time to pause
+	 */
+	private void pause(long time) {
+		startTime = System.currentTimeMillis();
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	/**
@@ -155,5 +166,28 @@ public class SplashScreen {
 		g.drawImage(icon.getImage(), 0, 0, width, height, null);
 		g.dispose();
 		return new ImageIcon(temp);
+	}
+
+	/**
+	 * Sets the parameters of the JFrame
+	 * 
+	 * @param size
+	 *            The size of the label
+	 * @param icon
+	 *            The icon for the frame
+	 * @return A JFrame
+	 */
+	private JFrame setupUI(Dimension size, ImageIcon icon) {
+		JFrame frame = new JFrame("Made By Inderpreet Dhillon");
+		frame.setAlwaysOnTop(true);
+		frame.setBackground(Color.BLACK);
+		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		frame.setIconImage(icon.getImage());
+		frame.setResizable(false);
+		frame.setSize(size);
+		frame.setUndecorated(true);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		return frame;
 	}
 }

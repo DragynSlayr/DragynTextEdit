466214a4fea44177cbc41c2da01d3d8d089e7a39
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-03-25 01:40:07 -0600

    Implemented custom scrollbars

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 /**
  *
@@ -12,6 +13,7 @@ public class Main {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
+				UIManager.put("ScrollBarUI", CustomScrollBarUI.class.getName());
 				new SplashScreen(2000);
 				new GUI();
 			}

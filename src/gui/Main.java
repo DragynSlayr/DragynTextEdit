8cb706f86da6fe29bb7696ae583f9c1567a2b3a6
Inderpreet Dhillon <inderpreet_dhillon@hotmail.com>
2015-04-12 22:47:37 -0600

    Removed an unused import

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,9 +1,5 @@
 package gui;
 
-import java.awt.Toolkit;
-
-import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
